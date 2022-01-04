cc2d16a349a5e647e7b31f5f07a2ee8739564fad
daonghia2404 <daonghia2404@gmail.com>
2022-01-04 18:57:29 +0700

    Fix reivew

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 window.onload = () => {
   header.init()
+  owlCarousel.init()
 }
 
 const header = {
   init: function() {
     this.toggleMenu();
+    this.scrollShowOnTop();
   },
   toggleMenu: function() {
     const btnMenu = document.querySelector('.header-mobile-btn');
@@ -29,5 +31,84 @@ const header = {
 
     document.addEventListener('mousedown', listener);
     document.addEventListener('touchstart', listener);
+  },
+  scrollShowOnTop: function() {
+    const btnOnTop = document.querySelector('.btn-on-top')
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) btnOnTop.classList.add('active')
+      else btnOnTop.classList.remove('active')
+    })
+  },
+}
+
+const owlCarousel = {
+  init: function () {
+    this.setupCarouselSection3()
+    this.setupCarouselSection4()
+    this.setupCarouselSection5()
+  },
+  setupCarouselSection3: function () {
+    const $owl = $("#carousel-section-3").owlCarousel({
+      responsive: {
+        0: {
+          items: 1,
+        },
+        425: {
+          items: 1.5,
+        },
+      },
+      loop: true,
+      center: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      smartSpeed: 300,
+      dots: true,
+      nav: false,
+      margin: 12,
+    });
+  },
+  setupCarouselSection4: function () {
+    const $owl = $("#carousel-section-4").owlCarousel({
+      responsive: {
+        0: {
+          items: 1,
+        },
+        425: {
+          items: 1.5,
+        },
+      },
+      loop: true,
+      center: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      smartSpeed: 300,
+      dots: true,
+      nav: false,
+      margin: 12,
+    });
+  },
+  setupCarouselSection5: function () {
+    const $owl = $("#carousel-section-5").owlCarousel({
+      responsive: {
+        0: {
+          items: 1,
+        },
+        425: {
+          items: 1.5,
+        },
+      },
+      loop: true,
+      center: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      smartSpeed: 300,
+      dots: true,
+      nav: false,
+      margin: 12,
+    });
   },
 }
